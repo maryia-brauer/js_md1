69f57c00aabd911d58df541b8dc9b25facde2f5d
Maryia <maryia.brauer@gmail.com>
2023-10-18 23:32:36 +0300

    First and last I hope

---
+++ src/components/AboutMeComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>ABOUT ME</h1>
+    <h3>Name: {{ user.name }}</h3>
+    <h3>Surname: {{ user.surname }}</h3>
+    <h3>Code: {{ user.code }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Maryia",
+        surname: "Brauer",
+        code: "IT21057"
+      }
+    };
+  }
+};
+</script>
---
+++ src/components/NavigationComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul class="tabs">
+      <li :class="{ active: activeTab === 'home' }" @click="changeTab('home')">HOME</li>
+      <li :class="{ active: activeTab === 'about' }" @click="changeTab('about')">ABOUT ME</li>
+    </ul>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    activeTab: String
+  },
+  methods: {
+    changeTab(tab) {
+      this.$emit('tab-change', tab);
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul.tabs {
+  list-style-type: none;
+  padding: 0;
+}
+
+ul.tabs li {
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+ul.tabs li.active {
+  border-bottom: 2px solid white;
+  background-color: purple;
+}
+</style>
